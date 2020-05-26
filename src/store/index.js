68bc2ee46d82e5b38da88c9bd54a34427815cb53
TomLaczek <tomaszlaczek@hotmail.com>
2020-05-26 15:58:51 +0200

    add new intern
    function for adding new intern

--- src/store/index.js
+++ src/store/index.js
@@ -8,9 +8,12 @@ const url = "https://reqres.in/api/users"
 export default new Vuex.Store({
   state: {
     internList:[],
-
+    responseInformation:null,
   },
   mutations: {
+    LAST_RESPONSE_INFORMATION(state,responseData){
+      state.responseInformation = responseData;
+    },
     PUSH_DATA_TO_ARRAY(state,responseData){
       responseData.map((internData)=>{
         state.internList.push(internData)
@@ -22,6 +25,14 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    addNewIntern({commit},internData){
+      return axios
+        .post(url,internData)
+        .then((response)=>{
+          console.log(response)
+          commit("LAST_RESPONSE_INFORMATION",response)
+        })
+    },
     countPages({commit}){
       this.state.internList=[];
       return axios
--- src/views/AddIntern.vue
+++ src/views/AddIntern.vue
@@ -6,20 +6,45 @@
             </v-row>
             <v-row class="justify-center align-center">
                 <v-col cols="8">
-                    <v-text-field :label='$t("first_name")' clearable></v-text-field>
+                    <v-text-field v-model="name" :label='$t("first_name")' clearable></v-text-field>
                 </v-col>
                 <v-col cols="8">
-                    <v-text-field :label='$t("last_name")' clearable></v-text-field>
+                    <v-text-field v-model="lastName" :label='$t("last_name")' clearable></v-text-field>
                 </v-col>
                 <v-col cols="8">
-                    <v-text-field :label='$t("avatar")' clearable></v-text-field>
+                    <v-text-field v-model="avatar" :label='$t("avatar")' clearable></v-text-field>
                 </v-col>
             </v-row>
             <v-row class="justify-end align-center">
-                <v-btn outlined>
+                <v-btn outlined rounded @click="addNewIntern()">
                     <span>{{$t("submit")}}</span>
                 </v-btn>
             </v-row>
         </v-container>
     </div>
 </template>
+<script>
+export default {
+    name:'addingIntern',
+    data() {
+        return{
+            name:null,
+            lastName:null,
+            avatar:null,
+        };
+    },
+    methods:{
+        addNewIntern(){
+            let internData = {
+                internName: this.name,
+                internLastName: this.lastName,
+                internEmail: this.avatar
+            }
+            this.$store.dispath('addNewIntern')
+                .then(()=>{
+                    console.log('dodano')
+                })
+        }
+    }
+}
+</script>
